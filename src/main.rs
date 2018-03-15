eccdc4d446516eca78872502889574f8ef28a36d
Samuel Laurén <samuel.lauren@iki.fi>
2018-03-15 22:14:33 +0200

    Getting color from screen works

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use xcb::base::{Connection};
 use xcb::xproto;
 use xcb::base as xbase;
 
+type RGB = (u8, u8, u8);
+
 fn wait_for_location(conn: &Connection, root: xproto::Window) -> Result<Option<(i16, i16)>, Error> {
     let reply = xproto::grab_pointer(&conn,
                                      false,
@@ -41,7 +43,32 @@ fn wait_for_location(conn: &Connection, root: xproto::Window) -> Result<Option<(
         }
     };
     xproto::ungrab_pointer(&conn, xbase::CURRENT_TIME);
+    conn.flush();
     Ok(result)
+}
+
+fn window_color_at_point(conn: &Connection, window: xproto::Window, (x, y): (i16, i16))
+                         -> Result<RGB, Error> {
+    let geometry = xproto::get_geometry(conn, window).get_reply()?;
+    let reply = xproto::get_image(conn,
+                                  xproto::IMAGE_FORMAT_Z_PIXMAP as u8,
+                                  window,
+                                  geometry.x(),
+                                  geometry.y(),
+                                  geometry.width(),
+                                  geometry.height(),
+                                  std::u32::MAX)
+        .get_reply()?;
+    if reply.depth() != 24 {
+        // TODO: Figure out what to do with these
+        return Err(err_msg("Unsupported depth"));
+    }
+    let base = (x as usize * 4) + ((y as usize) * (geometry.width() as usize * 4));
+    let data = reply.data();
+    let b = data[base + 0];
+    let g = data[base + 1];
+    let r = data[base + 2];
+    Ok((r, g, b))
 }
 
 fn run() -> Result<(), Error> {
@@ -50,10 +77,10 @@ fn run() -> Result<(), Error> {
         .ok_or_else(|| err_msg("Could not find screen"))?;
     let root = screen.root();
 
-    if let Some((x, y)) = wait_for_location(&conn, root)? {
-        println!("{}, {}", x, y);
+    if let Some(point) = wait_for_location(&conn, root)? {
+        let (r, g, b) = window_color_at_point(&conn, root, point)?;
+        println!("#{:02x}{:02x}{:02x}", r, g, b);
     }
-
     Ok(())
 }
 
